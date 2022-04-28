4d6360ef950a40f6f80389dbaa21581cf0c2c50f
João Lucas de Sousa Martins <lucasbanana96@gmail.com>
2022-04-27 23:44:51 -0300

    create database

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,16 +1,25 @@
 const express = require('express');
+const crypto = require('crypto');
+
+const connection = require('./database/connection');
 
 const routes = express.Router();
 
-routes.post('/users',(request,response)=>{
-    const body = request.body;
+routes.post('/ongs', async (request,response)=>{
+    const {name, email , whatsapp, city, uf} = request.body;
     
-    console.log(body);
+    const id = crypto.randomBytes(4).toString('HEX');
 
-    return response.json({
-        evento:'semana omnistack',
-        nome:'lucas'
+    await connection('ongs').insert({
+        id,
+        name,
+        email,
+        whatsapp,
+        city,
+        uf,
     });
+
+    return response.json({id});
 });
 
 module.exports = routes;
